2694bbfbe97bd507eadc3a02d7ea276d0da07b1e
bgaughran <gaughran@gmail.com>
2017-11-09 23:57:02 +0000

    Test commit

--- contract_10.sol
+++ contract_10.sol
@@ -27,7 +27,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-
 contract OraclizeI {
     address public cbAddress;
     function query(uint _timestamp, string _datasource, string _arg) returns (bytes32 _id);
