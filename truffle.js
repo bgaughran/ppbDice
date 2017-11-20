46ab346747ed6031cc4240376c804352f36ea9d1
bgaughran <gaughran@gmail.com>
2017-11-20 19:37:25 +0000

    Remove build output produced by Truffle

--- truffle.js
+++ truffle.js
@@ -2,3 +2,13 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
+//
+//networks: {
+//  development: {
+//    host: "localhost",
+//    port: 8545,
+//    gas: 52342342342344712388,
+//    gasPrice: 52342342342344712388,
+//    network_id: "*" // match any network
+//  }
+//};
