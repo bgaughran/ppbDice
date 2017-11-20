3c4a718840eba1984ecb3237d6c97c45e119fb90
bgaughran <gaughran@gmail.com>
2017-11-20 22:50:54 +0000

    Adding truffle deployments

--- truffle.js
+++ truffle.js
@@ -1,14 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+//      gas: 6000000,
+      network_id: "*" // Match any network id
+    }
+  }
 };
-//
-//networks: {
-//  development: {
-//    host: "localhost",
-//    port: 8545,
-//    gas: 52342342342344712388,
-//    gasPrice: 52342342342344712388,
-//    network_id: "*" // match any network
-//  }
-//};
